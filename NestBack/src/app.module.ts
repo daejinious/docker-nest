a40645a3b9cd6bea3efc71110a1dc6c230bbaded
daejinious <size_chi@naver.com>
2023-01-04 18:51:28 +0900

    Board Module 생성

--- NestBack/src/app.module.ts
+++ NestBack/src/app.module.ts
@@ -1,30 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PostgreSqlConfigModule } from './config/PostgreSqlConfigModule.module';
-import { PostgreSqlConfigService } from './config/PostgreSqlConfigService.service';
-import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
-import { MySqlConfigModule } from './config/MySqlConfigModule.module';
-import { MySqlConfigService } from './config/MySqlConfigService.service';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [MySqlConfigModule],
-      useClass: MySqlConfigService,
-      inject: [MySqlConfigService],
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [PostgreSqlConfigModule],
-      useClass: PostgreSqlConfigService,
-      inject: [PostgreSqlConfigService],
-    }),
-    UserModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BoardsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
