25471d90b643f5f09d3faedf2ab17e815000600d
daejinious <size_chi@naver.com>
2023-01-05 10:08:42 +0900

    Board 생성 처리

--- NestBack/src/boards/boards.controller.ts
+++ NestBack/src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-import { BoardsService } from './boards.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
@@ -9,5 +9,13 @@ export class BoardsController {
   @Get()
   getAll(): Board[] {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardsService.createBoard(title, description);
   }
 }
