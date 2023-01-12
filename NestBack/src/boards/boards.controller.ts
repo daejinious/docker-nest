3ce5999cc3baf6ea0f79f04bcc6ad91ef6484bf7
daejinious <size_chi@naver.com>
2023-01-12 16:25:18 +0900

    DB 연동을 위한 source 초기화

--- NestBack/src/boards/boards.controller.ts
+++ NestBack/src/boards/boards.controller.ts
@@ -1,50 +1,37 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { Controller } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import CreateBoardDto from './dto/create-board.dto';
-import {BoardStatusValidationPipe} from "./pipes/board-status-validation.pipe";
 
 @Controller('boards')
 export class BoardsController {
   constructor(private readonly boardsService: BoardsService) {}
-
-  @Get()
-  getAll(): Board[] {
-    return this.boardsService.getAllBoards();
-  }
-
-  @Post()
-  @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    return this.boardsService.createBoard(createBoardDto);
-  }
-
-  @Get('/:id')
-  getBoardById(@Param('id') boardId): Board {
-    return this.boardsService.getBoardById(boardId);
-  }
-
-  @Delete('/:id')
-  deleteBoard(@Param('id') boardId: string): Board[] {
-    this.boardsService.removeBoard(boardId);
-    return this.boardsService.getAllBoards();
-  }
-
-  @Patch('/:id/status')
-  updateBoardStatus(
-    @Param('id') boardId: string,
-    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ): Board {
-    return this.boardsService.updateBoardStatus(boardId, status);
-  }
+  //
+  // @Get()
+  // getAll(): Board[] {
+  //   return this.boardsService.getAllBoards();
+  // }
+  //
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  //   return this.boardsService.createBoard(createBoardDto);
+  // }
+  //
+  // @Get('/:id')
+  // getBoardById(@Param('id') boardId): Board {
+  //   return this.boardsService.getBoardById(boardId);
+  // }
+  //
+  // @Delete('/:id')
+  // deleteBoard(@Param('id') boardId: string): Board[] {
+  //   this.boardsService.removeBoard(boardId);
+  //   return this.boardsService.getAllBoards();
+  // }
+  //
+  // @Patch('/:id/status')
+  // updateBoardStatus(
+  //   @Param('id') boardId: string,
+  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  // ): Board {
+  //   return this.boardsService.updateBoardStatus(boardId, status);
+  // }
 }
--- NestBack/src/boards/boards.service.ts
+++ NestBack/src/boards/boards.service.ts
@@ -1,48 +1,43 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
-import { v1 as uuid } from 'uuid';
-import CreateBoardDto from './dto/create-board.dto';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BoardsService {
-  private boards: Board[] = [];
-
-  getAllBoards(): Board[] {
-    return this.boards;
-  }
-
-  createBoard(createBoardDto: CreateBoardDto) {
-    const { title, description } = createBoardDto;
-    const board: Board = {
-      id: uuid(),
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    };
-
-    this.boards.push(board);
-    return board;
-  }
-
-  getBoardById(id: string): Board {
-    const board = this.boards.find((board) => id === board.id);
-
-    if (!board) {
-      throw new NotFoundException(`게시물을 찾을 수 없습니다.`);
-    }
-
-    return board;
-  }
-
-  removeBoard(id: string): void {
-    const found = this.getBoardById(id);
-    this.boards = this.boards.filter((board) => found.id === board.id);
-  }
-
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    const board: Board = this.getBoardById(id);
-    board.status = status;
-
-    return board;
-  }
+  // getAllBoards(): Board[] {
+  //   return this.boards;
+  // }
+  //
+  // createBoard(createBoardDto: CreateBoardDto) {
+  //   const { title, description } = createBoardDto;
+  //   const board: Board = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: BoardStatus.PUBLIC,
+  //   };
+  //
+  //   this.boards.push(board);
+  //   return board;
+  // }
+  //
+  // getBoardById(id: string): Board {
+  //   const board = this.boards.find((board) => id === board.id);
+  //
+  //   if (!board) {
+  //     throw new NotFoundException(`게시물을 찾을 수 없습니다.`);
+  //   }
+  //
+  //   return board;
+  // }
+  //
+  // removeBoard(id: string): void {
+  //   const found = this.getBoardById(id);
+  //   this.boards = this.boards.filter((board) => found.id === board.id);
+  // }
+  //
+  // updateBoardStatus(id: string, status: BoardStatus): Board {
+  //   const board: Board = this.getBoardById(id);
+  //   board.status = status;
+  //
+  //   return board;
+  // }
 }
