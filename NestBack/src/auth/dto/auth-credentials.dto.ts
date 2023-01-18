deb77a78d3c7f0b681630b1995a4906e01a78fa1
daejinious <size_chi@naver.com>
2023-01-18 09:54:24 +0900

    Dto에 validation check decorator 추가

--- NestBack/src/auth/dto/auth-credentials.dto.ts
+++ NestBack/src/auth/dto/auth-credentials.dto.ts
@@ -1,5 +1,16 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(40)
   userId: string;
+
+  @IsString()
   username: string;
+
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(40)
   password: string;
 }
