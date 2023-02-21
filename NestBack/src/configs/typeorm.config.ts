16a2f76acf15f10a63fcc4597120140f3ca5444a
daejinious <size_chi@naver.com>
2023-02-21 10:01:26 +0900

    [Modify] DB 정보 env 파일에서 load하도록 수정

--- NestBack/src/configs/typeorm.config.ts
+++ NestBack/src/configs/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'db',
-  port: 5432,
-  username: 'commercelab',
-  password: 'commercelab',
-  database: 'commercelab',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.DB,
   entities: [__dirname + '/../**/*.entity.{ts,js}'],
   synchronize: true,
 };
